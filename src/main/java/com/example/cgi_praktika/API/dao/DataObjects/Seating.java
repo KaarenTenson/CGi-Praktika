4b1d981c3e58f0e51a0b1051031e0bcd269c973d
KaarenTenson <tensonkaaren@gmail.com>
2025-03-10 18:57:59 +0200

    added windows to the seating page

--- src/main/java/com/example/cgi_praktika/API/dao/DataObjects/Seating.java
+++ src/main/java/com/example/cgi_praktika/API/dao/DataObjects/Seating.java
@@ -2,11 +2,15 @@ package com.example.cgi_praktika.API.dao.DataObjects;
 
 public class Seating {
     private final Seat[][] PlaneStructure;
-
-    public Seating(Seat[][] planeStructure) {
+    private final int[][] windows;
+    public Seating(Seat[][] planeStructure, int[][] windows) {
         PlaneStructure = planeStructure;
+        this.windows = windows;
     }
     public Seat[][] getPlaneStructure() {
         return PlaneStructure;
+    }
+    public int[][] getWindows() {
+        return windows;
     }
 }
